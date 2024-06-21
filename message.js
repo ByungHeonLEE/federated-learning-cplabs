239e631c9dd9b1a1f501be60aeb18b827e415c12
Eric <59383683+ByungHeonLEE@users.noreply.github.com>
2024-06-21 10:48:58 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,17 +1,18 @@
 async function userOpMessage(params) {
-  return {
+  const userOp = {
     sender: params.sender,
     nonce: params.nonce,
-    initCode: params.init_code,
-    callData: params.call_data,
+    initCode: (params.init_code) || '0x',
+    callData: (params.call_data) || '00',
     callGasLimit: params.call_gas_limit,
     verificationGasLimit: params.verification_gas_limit,
     preVerificationGas: params.pre_verification_gas,
     maxFeePerGas: params.max_fee_per_gas,
     maxPriorityFeePerGas: params.max_priority_fee_per_gas,
-    paymasterAndData: params.paymaster_and_data,
-    signature: params.signature
+    paymasterAndData: (params.paymaster_and_data),
+    signature: (params.signature)
   };
+  return userOp;
 }
 
 // Expose the function to be executed by server
